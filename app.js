ac1c268a58279e7480e5dd2f57813bcda304bf14
karan <jangdekaran54@gmail.com>
2025-01-03 12:16:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 4000;
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ 
+  origin: "*", 
+  methods: "GET,HEAD,PUT,POST,DELETE",
+  credentials: true 
+}));
 
 // Require Routes
 const userRoutes = require("./routes/UserRoutes");
